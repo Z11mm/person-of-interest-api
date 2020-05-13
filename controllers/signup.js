f5a647d18689794f4bdec2866539589a60049112
Z11mm <zimobiechina@gmail.com>
2020-05-13 13:11:00 +0100

    Refactor codebase
    Move signup endpoint into controllers directory

---
+++ controllers/signup.js
@@ -0,0 +1,32 @@
+const handleSignUp = (req, res, dB, bcrypt) => {
+  const { email, password, name } = req.body;
+  const hash = bcrypt.hashSync(password);
+
+  dB.transaction(trx => {
+    trx
+      .insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            name: name,
+            email: loginEmail[0],
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch(err => res.status(400).json('unable to sign up'));
+};
+
+module.exports = {
+  handleSignUp: handleSignUp
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const signup = require('./controllers/signup')
 
 const dB = knex({
   client: 'pg',
@@ -47,34 +48,7 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('wrong credentials'));
 });
 
-app.post('/signup', (req, res) => {
-  const { email, password, name } = req.body;
-  const hash = bcrypt.hashSync(password);
-
-  dB.transaction(trx => {
-    trx
-      .insert({
-        hash: hash,
-        email: email
-      })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            name: name,
-            email: loginEmail[0],
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(err => res.status(400).json('unable to sign up'));
-});
+app.post('/signup', (req, res) => { signup.handleSignUp(req, res, dB, bcrypt) });
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
